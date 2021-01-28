b733fabe8625c97c683f16832b3825918ee5eb34
sow23 <gangadarisowmya@gmail.com>
2021-01-28 12:43:35 -0600

    Initial Version- added findBYValue and added printJobs

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -54,15 +54,37 @@ public class JobData {
         return allJobs;
     }
 
+    public static ArrayList<HashMap<String, String>> findByValue(String[] columns, String value) {
+
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        for (String column : columns) {
+            if (!column.equalsIgnoreCase("all")) {
+                for (HashMap<String, String> row : allJobs) {
+
+                    String aValue = row.get(column);
+
+                    if (aValue.equalsIgnoreCase(value)) {
+                        jobs.add(row);
+                    }
+                }
+            }
+        }
+        return jobs;
+    }
+
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -76,7 +98,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.equalsIgnoreCase(value)) {
                 jobs.add(row);
             }
         }
